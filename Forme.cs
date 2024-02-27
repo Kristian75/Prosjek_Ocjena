bc67563a2ba98a47063f22ae9a26833e98729037
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2024-02-27 12:18:38 +0100

    Add files via upload

---
+++ Forme.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProsjekOcjena
+{
+    public partial class Form1 : Form
+    {
+        int ocjena, brojOcjena, zbroj;
+        double prosjek;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UnesiOcjenu_Click(object sender, EventArgs e)
+        {
+            ocjena = Convert.ToInt32(txtUnosOcjena.Text);
+            brojOcjena++;
+            zbroj += ocjena;
+            DialogResult odgovor = MessageBox.Show("Želiš li upisati još ocjena?", "Upit",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            switch (odgovor)
+            {
+                case DialogResult.Yes:
+                    txtUnosOcjena.Clear();
+                    txtUnosOcjena.Focus();
+                    break;
+                    case DialogResult.No:
+                    prosjek = (double)zbroj / brojOcjena;
+                    txtProsjek.Text = prosjek.ToString();
+                    break;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
